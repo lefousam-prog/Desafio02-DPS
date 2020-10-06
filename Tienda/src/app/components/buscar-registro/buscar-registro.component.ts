cc3f4566c86342b6d0fe3876205cc1b7ce9eb1e7
Taru <joshuacalebreyes77@gmail.com>
2020-10-05 23:20:17 -0600

    Desafio 2 finalizado

--- Tienda/src/app/components/buscar-registro/buscar-registro.component.ts
+++ Tienda/src/app/components/buscar-registro/buscar-registro.component.ts
@@ -1,7 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NgForm } from '@angular/forms';
+
 //Service
 import { AuthService } from "../../services/auth.service";
+
+import { Shopping } from '../../models/shopping';
+
+import { Products } from '../../models/products';
+
+import { ShoppingService } from '../../services/shopping.service';
+
+import { ProductsService } from '../../services/products.service';
+
+import { ToastrService } from 'ngx-toastr';
+import { element } from 'protractor';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-buscar-registro',
@@ -9,10 +23,118 @@ import { AuthService } from "../../services/auth.service";
   styleUrls: ['./buscar-registro.component.css']
 })
 export class BuscarRegistroComponent implements OnInit {
+  public optionsearch: string;
+  public keyword: string;
+  productsList: Products[];
+  shoppingList: Shopping[];
 
-  constructor(public authService: AuthService) { }
+  constructor(
+    public productsService: ProductsService,
+    public shoppingService: ShoppingService,
+    public toastr: ToastrService,
+    public authService: AuthService
+  ) { 
+  }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.optionsearch = "0";
+    this.keyword = "";
+    this.shoppingService.getShopping();
+
+    this.productsService.getProducts().snapshotChanges().subscribe(item => {
+      this.productsList = [];
+      item.forEach(element => {
+        let x = element.payload.toJSON();
+        x["$key"] = element.key;
+        this.productsList.push(x as Products);
+      });
+    });
+
+    this.shoppingService.getShopping().snapshotChanges().subscribe(item => {
+      this.shoppingList = [];
+      item.forEach(element => {
+        let x = element.payload.toJSON();
+        x["$key"] = element.key;
+        this.shoppingList.push(x as Shopping);
+      });
+    });
+
+    this.resetForm();
+  }
+  
+  onSubmit(searchForm: NgForm){
+    let valopt = this.optionsearch;
+    let word = this.keyword;
+    let stringP = "";
+    let stringS = "";
+    this.productsList.forEach(function(value){
+      switch (valopt){
+        case "0":
+          if(word == value.$key){ 
+            stringP = stringP + "<tr><td>"+value.$key+"</td><td>"+value.code_product+"</td><td>"+value.description_product+"</td><td>"+value.price+"</td></tr>";
+          }
+          break;
+        case "3":
+          if(word == value.code_product){ 
+            stringP = stringP + "<tr><td>"+value.$key+"</td><td>"+value.code_product+"</td><td>"+value.description_product+"</td><td>"+value.price+"</td></tr>";
+          }
+          break;
+        case "4":
+          if(word == value.description_product){ 
+            stringP = stringP + "<tr><td>"+value.$key+"</td><td>"+value.code_product+"</td><td>"+value.description_product+"</td><td>"+value.price+"</td></tr>";
+          }
+          break;
+        case "5":
+          if(word == value.price.toString()){ 
+            stringP = stringP + "<tr><td>"+value.$key+"</td><td>"+value.code_product+"</td><td>"+value.description_product+"</td><td>"+value.price+"</td></tr>";
+          }
+          break;
+      }
+    });
+
+    document.getElementById("registrosP").innerHTML = stringP;
+
+    this.shoppingList.forEach(function(value){
+      switch (valopt){
+        case "0":
+          if(word == value.$key){ 
+            stringS = stringS + "<tr><td>"+value.$key+"</td><td>"+value.name_client+"</td><td>"+value.dui_client+"</td><td>"+value.code_product+"</td><td>"+value.description_product+"</td><td>"+value.price+"</td></tr>";
+          }
+          break;
+        case "1":
+          if(word == value.name_client){ 
+            stringS = stringS + "<tr><td>"+value.$key+"</td><td>"+value.name_client+"</td><td>"+value.dui_client+"</td><td>"+value.code_product+"</td><td>"+value.description_product+"</td><td>"+value.price+"</td></tr>";
+          }
+          break;
+        case "2":
+          if(word == value.dui_client){ 
+            stringS = stringS + "<tr><td>"+value.$key+"</td><td>"+value.name_client+"</td><td>"+value.dui_client+"</td><td>"+value.code_product+"</td><td>"+value.description_product+"</td><td>"+value.price+"</td></tr>";
+          }
+          break;
+        case "3":
+          if(word == value.code_product){ 
+            stringS = stringS + "<tr><td>"+value.$key+"</td><td>"+value.name_client+"</td><td>"+value.dui_client+"</td><td>"+value.code_product+"</td><td>"+value.description_product+"</td><td>"+value.price+"</td></tr>";
+          }
+          break;
+        case "4":
+          if(word == value.description_product){ 
+            stringS = stringS + "<tr><td>"+value.$key+"</td><td>"+value.name_client+"</td><td>"+value.dui_client+"</td><td>"+value.code_product+"</td><td>"+value.description_product+"</td><td>"+value.price+"</td></tr>";
+          }
+          break;
+        case "5":
+          if(word == value.price.toString()){ 
+            stringS = stringS + "<tr><td>"+value.$key+"</td><td>"+value.name_client+"</td><td>"+value.dui_client+"</td><td>"+value.code_product+"</td><td>"+value.description_product+"</td><td>"+value.price+"</td></tr>";
+          }
+          break;
+      }
+    });
+
+    document.getElementById("registrosS").innerHTML = stringS;
+  }
+
+  resetForm(searchForm?: NgForm) {
+    if(searchForm != null)
+    searchForm.reset();
   }
 
 }
--- Tienda/src/app/components/shopping-history/shopping-list/shopping-list.component.ts
+++ Tienda/src/app/components/shopping-history/shopping-list/shopping-list.component.ts
@@ -7,6 +7,10 @@ import { ShoppingService } from '../../../services/shopping.service';
 import { ToastrService } from 'ngx-toastr';
 import { element } from 'protractor';
 
+import { jsPDF } from "jspdf";
+
+import * as printJS from 'print-js';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
@@ -41,6 +45,28 @@ export class ShoppingListComponent implements OnInit {
       this.shoppingService.deleteShopping($key);
       this.toastr.success("Registro eliminado", "La compra se ha eliminado de forma satisfactoria");
     }
+  }
+
+  public generarTicket(shoppingprint: Shopping){
+
+    
+    printJS({
+      printable: [{
+        "ID de la compra" : shoppingprint.$key,
+        "Código del producto" : shoppingprint.code_product,
+        "Nombre del producto" : shoppingprint.description_product,
+        "Dui del cliente" : shoppingprint.dui_client,
+        "Nombre del cliente" : shoppingprint.name_client,
+        "Precio cancelado (Incluye descuento si aplica)" : shoppingprint.price
+      }],
+      properties: ['ID de la compra', 'Nombre del cliente', 'Dui del cliente', 'Código del producto', 'Nombre del producto', 'Precio cancelado (Incluye descuento si aplica)'],
+      type: 'json',
+      header: 'Ticket de compra (Tienda Don Diego)'
+    });
+    /*
+    const doc = new printJS();
+    doc.html("<center><h1>Ticket de compra de producto</h1></center> <p>Nombre del cliente: <b>"+shoppingprint.name_client+"</b></p><br/>  <p>Dui del cliente: <b>"+shoppingprint.dui_client+"</b></p><br/><br/> <table border='1'><tr><td>Código del producto</td><td>Nombre del producto</td><td>Precio del producto (Descuento aplicado)</td></tr><tr><td>"+shoppingprint.code_product+"</td><td>"+shoppingprint.description_product+"</td><td>"+shoppingprint.price+"</td></tr></table> <center><h3>¡La compra se realizo de forma satisfactoria!</h3></center>");
+    doc.save("ticket_de_compra.pdf");*/
   }
 
 }
